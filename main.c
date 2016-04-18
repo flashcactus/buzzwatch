9bea210001c88f1e199d8cc0f540a0a112777273
Ivan Uvarov <flashcactus@ya.ru>
2016-04-19 00:40:08 +0300

    some button-processing code.

--- main.c
+++ main.c
@@ -14,15 +14,26 @@
 #define POT_FMASK ~((~0) << POT_FREQ) //00..01..1, POT_FREQ ones
 #define BUZZ_PATT 0b1101
 
+//times for clicks (in ovf_ticks)
+#define CLICK_S 1      //65ms must be enough
+#define CLICK_M 15     //just under 1s
+#define CLICK_L 46     //just over 3s        
+#define CLICK_XXL 152   //10s
+#define MINRELEASE 2   //~130 ms
+#define MAXRELEASE 16  //~1s
+
 //globals
-uint8_t prescale_mask=0;
-uint8_t prescale_ctr;
-uint8_t delay_ctr;
-uint8_t ovf_ticks; //counts timer overflows [see timsetup()]
+uint8_t prescale_mask=0;//1<<exp
+uint8_t prescale_ctr;   //when this reaches 2^exp, delay_ctr is incremented
+uint8_t delay_ctr;      //this gets compared with the ADC value
+uint8_t ovf_ticks;      //counts timer overflows [see timsetup()]
 
-uint8_t pot_value;
+uint8_t pot_value;      //whatever we got from the ADC
+uint8_t btn_states;     //previous states of the button (will probably be 10101010... but whatever)
 
-uint16_t buzz_pattern;
+uint8_t shortclick_cnt; //interestingly enough, counts small clicks.
+
+uint16_t buzz_pattern;  //buzz a rhythm!
 
 
 inline void timzero(){
@@ -37,6 +48,7 @@ inline void timsetup() {
                             //  tick=.25 ms; 
                             //  overflow=65.5ms
                             //  1s=15.25ovf;  
+                            //  ovf_ticks overflow=16.77s
     
     TIMSK0 |= (1<<TOIE0);        //enable the interrupt
     timzero();
@@ -74,14 +86,19 @@ int main(void)
             //0; //speed up 64x for debug
     
     //set i/o pin modes
-    DDRB |= (1<<POT_ENABLE_PIN) | (1<<BUZZ_PIN) | (1<<SHIFT_PIN);  //out
-    DDRB &= ~(1<<POT_PIN) & ~(1<<BTN_PIN);  //in
-    
+    DDRB |= (1<<POT_ENABLE_PIN) | (1<<BUZZ_PIN) | (1<<SHIFT_PIN);   //out
+    DDRB &= ~(1<<POT_PIN) & ~(1<<BTN_PIN);                          //in
+    PORTB |= (1<<BTN_PIN);  //enable the internal pullup for the button
+
     //setup the timer
     timsetup();
 
     //setup the ADC
     adcsetup();
+
+    //enable the INT0 interrupt for button capture
+    EIMSK |= (1<<INT0);                 //enable
+    MCUCR &= ~((1<<ISC01)|(1<<ISC00));  //set INT0 mode to level-triggered
 
     //setup sleep
     //we want the deepest (powerdown) sleep mode 
@@ -91,25 +108,28 @@ int main(void)
     delay_ctr = 0;
     buzz_pattern = 0;
     
-    
     //########## setup complete; re-enable interrupts ##########
-
     sei();
     
-    //start main loop
+    //do the sleepyloop
     while(1) {
         if (                        //sleep-allowed conditions (all at once):
             !(ADCSRA & (1<<ADSC))   //  ADC not currently converting
-            && MCUCR & (1<<SE)      //  sleep-enable bit is set (WDT routine finished)
+            && MCUCR & (1<<SE)      //  sleep-enable bit is set (=WDT routine finished)
             && !buzz_pattern        //  buzzed everything already
+            && PORTB & (1<<BTN_PIN) //  button is not pressed
         ){
+            cli(); 
             //turn everything off, to be sure
             PORTB &= ~( (1<<BUZZ_PIN) | (1<<POT_ENABLE_PIN) ) ;
             
             // Disable BOD
-            cli(); 
             sleep_bod_disable();    
             sei(); 
+            
+            //set INT0 mode to level-triggered 
+            //(otherwise it couldn't wake the MCU)
+            MCUCR &= ~((1<<ISC01)|(1<<ISC00));  
             
             // Go to Sleep
             sleep_cpu();            
@@ -117,6 +137,17 @@ int main(void)
     } 
 }
 
+ISR(INT0_vect){
+    MCUCR |= (1<<ISC00);        //set edge-triggered mode
+    btn_states <<= 1;
+    if(! PORTB & (1<<BTN_PIN)) {//button is pressed
+        btn_states |= 1;
+    }
+    /*switch(btn_states&0b11){
+        case 0b01:  //release
+        case 0b10:  //press
+    }*/
+}
 
 ISR(WDT_vect){
     sleep_disable(); 
@@ -146,20 +177,36 @@ ISR(WDT_vect){
 
 ISR(TIM0_OVF_vect){
     ++ovf_ticks;
-    shift_out_1w(ovf_ticks, &PORTB, 1<<SHIFT_PIN, 10);
+
+    shift_out_1w(ovf_ticks, &PORTB, 1<<SHIFT_PIN, 10); //debug
     shift_out_1w(delay_ctr, &PORTB, 1<<SHIFT_PIN, 10);
-    if(buzz_pattern&1) {
-        PORTB |= (1<<BUZZ_PIN);   //turn on
+
+    if(buzz_pattern&1) {            //bzzzzz!
+        PORTB |= (1<<BUZZ_PIN);
     } else {
-        PORTB &= ~(1<<BUZZ_PIN);  //turn off
+        PORTB &= ~(1<<BUZZ_PIN);
     }
     buzz_pattern = buzz_pattern>>1;
-}
 
+    //do some button-processing
+    if (PORTB & (1<<BTN_PIN)){          //button is released
+        if(ovf_ticks >= MAXRELEASE) {   //and has been for some time now
+            //TODO: finalize buttonstats
+        }
+    } else {                            //button is pressed
+        //do a small buzz at each of the thresholds
+        if (ovf_ticks == CLICK_M || ovf_ticks == CLICK_L) { 
+            PORTB |= (1<<BUZZ_PIN);
+        }
+        //long long press = reset.
+        else if (ovf_ticks >= CLICK_XXL) {  
+            //TODO: soft-reset the MCU
+        }
+    }
+}
 
 ISR(ADC_vect){                      //conversion complete
     PORTB &= ~(1<<POT_ENABLE_PIN);  //turn the pot off
     pot_value = ADCH;               //get the value
     ADCSRA &= ~(1<<ADEN);           //kill the ADC
-    //wait_len = pot_value;
 }
